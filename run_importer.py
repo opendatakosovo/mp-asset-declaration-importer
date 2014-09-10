355891c2c68af19e28c03ec39074e81e6d2a03a3
legeorges <georges.labreche@gmail.com>
2014-09-10 04:58:01 -0700

    Implemented default value for csv directory path

--- run_importer.py
+++ run_importer.py
@@ -3,7 +3,10 @@ import argparse
 from mpadi import import_data
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--csvDirectoryPath', type=str, help='The CSV directory path.')
+parser.add_argument(
+    '--csvDirectoryPath',
+    type=str, default='data',
+    help='The CSV directory path.')
 
 args = parser.parse_args()
 csv_directory_path = args.csvDirectoryPath
@@ -11,23 +14,20 @@ csv_directory_path = args.csvDirectoryPath
 
 # Run the app
 if __name__ == '__main__':
-	result_dict = {}
+    result_dict = {}
 
-	if os.path.isdir(csv_directory_path):
-		for filename in os.listdir(csv_directory_path):
-			if(filename.endswith(".csv")):
+    if os.path.isdir(csv_directory_path):
+        for filename in os.listdir(csv_directory_path):
+            if(filename.endswith(".csv")):
 
-				csv_filepath = csv_directory_path + '/' + filename
-				year = int(filename.replace('.csv', ''))
+                csv_filepath = csv_directory_path + '/' + filename
+                year = int(filename.replace('.csv', ''))
 
-				print '\n\nImporting from %s\n' % csv_filepath
-				doc_count = import_data(csv_filepath, year)
+                print '\n\nImporting from %s\n' % csv_filepath
+                doc_count = import_data(csv_filepath, year)
 
-				result_dict[filename] = doc_count
+                result_dict[filename] = doc_count
 
-	print "\nResults:"
-	for key in result_dict.keys():
-		print "%s Kosovo Assembly Deputies from '%s'" % (result_dict[key], key)
-
-	
-
+    print "\nResults:"
+    for key in result_dict.keys():
+        print "%s Kosovo Assembly Deputies from '%s'" % (result_dict[key], key)
